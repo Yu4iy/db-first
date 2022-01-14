5859a00c5fffe670488143469fbd3ab5d27df94c
Yu4iy <yurimohyliuk@gmail.com>
2022-01-14 10:05:53 +0100

    fix

--- Query.sql
+++ Query.sql
@@ -3,7 +3,13 @@
 SELECT * 
 FROM `students` 
 WHERE `date_of_birth` 
-LIKE '1990%'; 
+LIKE '1990%';
+
+SELECT * 
+FROM `students` 
+WHERE 
+YEAR(`date_of_birth` ) = 1990; 
+
 
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
@@ -18,6 +24,11 @@ FROM `students`
 WHERE `date_of_birth` 
 BETWEEN "1992-01-01" 
 AND '2021-12-31';
+
+
+SELECT * 
+FROM `students`
+WHERE `date_of_birth` < DATE_SUB(CURENT_DATE(), INTERVAL 31 YEAR);
 
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
@@ -43,6 +54,11 @@ BETWEEN "14:00"
 AND "24:00";
 
 
+SELECT * 
+FROM `exams` 
+WHERE `date` = "2020-06-10" 
+AND HOUR (`hour`) >= 14 
+
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 
 SELECT * 
@@ -56,12 +72,12 @@ LIKE "%Magistrale%";
 
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
-SELECT COUNT(`id`) 
+SELECT COUNT(`id`)  AS 'department_count'
 FROM `departments`; 
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
-SELECT * 
+SELECT COUNT(`id`) AS `phone_number`
 FROM `teachers` 
 WHERE `phone` 
 IS NULL;
